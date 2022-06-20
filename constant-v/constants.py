80238f42ea1f6e1b6fe5dc76eb009fd0659e351e
HuntFeng <spacecurvature@gmail.com>
2022-06-20 10:17:56 -0700

    contant-v results organized

---
+++ constant-v/constants.py
@@ -0,0 +1,7 @@
+# The following constants are in SI unit
+eps0 = 8.8510e-12 # vacuum permeability
+mu0 = 1.2566e-6 # magnetic permitivity
+kb = 1.3800e-23 # boltzmann constant
+me = 9.1040e-31 # electron mass
+c = 3*10**8 # speed of light
+q = 1.6022e-19 # fundamental charge
---
+++ matplotlib_settting.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+plt.rcParams["figure.figsize"] = (10,10)
+plt.rcParams['figure.facecolor'] = 'white'
+plt.rcParams["figure.titlesize"] = 20
+plt.rcParams["axes.titlesize"] = 20
+plt.rcParams["axes.labelsize"] = 18
+plt.rcParams["xtick.labelsize"] = 18
+plt.rcParams["ytick.labelsize"] = 18
+plt.rcParams['axes.facecolor'] = 'white'
+plt.rcParams["legend.fontsize"] = 16
+plt.rcParams['savefig.facecolor'] = 'white'
